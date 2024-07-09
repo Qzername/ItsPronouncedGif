f5e651a86c2c424fd26bf6cf8938edbc63870ae5
Hubert <40564309+Qzername@users.noreply.github.com>
2024-07-09 23:44:28 +0200

    analizing bug + debug tool

--- ItsPronouncedGif/ViewModels/MainViewModel.cs
+++ ItsPronouncedGif/ViewModels/MainViewModel.cs
@@ -1,7 +1,7 @@
 ﻿using Avalonia.Media;
+using Avalonia.Media.Imaging;
 using ItsPronouncedGif.ScreenInteractions;
 using ReactiveUI.Fody.Helpers;
-using System.Diagnostics;
 
 namespace ItsPronouncedGif.ViewModels;
 
@@ -11,6 +11,9 @@ public class MainViewModel : ViewModelBase
     [Reactive] int height { get; set; } = 493;
     [Reactive] int x { get; set; } = 100;
     [Reactive] int y { get; set; } = 100;
+
+    [Reactive] Bitmap? result { get; set; }
+    [Reactive] bool showResultError { get; set; } = false;
 
     [Reactive] bool isTextBoxesEnabled { get; set; } = true;
 
@@ -30,12 +33,31 @@ public class MainViewModel : ViewModelBase
             isTextBoxesEnabled = false;
         }
 
-        gif.AddPicture(screen.CaptureScreen(x, y, width, height));
+        //gif.AddPicture(screen.CaptureScreen(x, y, width, height));
+
+        Color[,] pic = new Color[width, height];
+
+        for (int x = 0; x < width; x++)
+            for (int y = 0; y < height; y++)
+                pic[x, y] = Color.FromRgb((byte)(x<255? x/(float)width*255 : 255) , 255, 255);
+
+        gif.AddPicture(pic);
     }
 
     public void Compile()
     {
+        showResultError = false;
+
         gif.Compile("./result.gif");
         isTextBoxesEnabled = true;
+
+        try
+        {
+            result = new Bitmap("./result.gif");
+        }
+        catch(System.Exception)
+        {
+            showResultError= true;
+        }
     }
 }
